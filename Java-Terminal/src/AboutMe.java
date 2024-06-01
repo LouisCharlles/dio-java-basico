562d6c4d5847291f0a34977e3ab466dc05de61f3
Luis Carlos Macêdo Moreira <luiscmacedo84@gmail.com>
2024-06-01 15:10:14 -0300

    Adicione scanner e modifica classe aboutme e deleta launch.json

--- Java-Terminal/src/AboutMe.java
+++ Java-Terminal/src/AboutMe.java
@@ -1,14 +1,29 @@
-//As informações estão sendo lidas pelas configurações do launch.json
+import java.util.Locale;
+import java.util.Scanner;
+
 public class AboutMe {
-    public static void main(String[] args){
+    public static void main(String[] args) {
+        //criando o objeto scanner
+        Scanner scanner = new Scanner(System.in).useLocale(Locale.US);
+        
+        System.out.println("Digite seu nome");
+        String nome = scanner.next();
+        
+        System.out.println("Digite seu sobrenome");
+        String sobrenome = scanner.next();
 
-        String nome = args[0];
-        String sobrenome = args[1];
-        int idade = Integer.valueOf(args[2]);
-        double altura = Double.valueOf(args[3]);
+        System.out.println("Digite sua idade");
+        int idade = scanner.nextInt();
+        
+        System.out.println("Digite sua altura");
+        double altura = scanner.nextDouble();
 
-        System.out.println("Olá, me chamo "+nome+" "+sobrenome);
-        System.out.println("Tenho "+idade+" anos de idade.");
-        System.out.println("Tenho "+altura+" cm de altura.");
+        
+        //imprimindo os dados obtidos pelo usuario
+        System.out.println("Ola, me chamo " + nome + " " + sobrenome);
+        System.out.println("Tenho " + idade + " anos ");
+        System.out.println("Minha altura é " + altura + "cm ");
+        scanner.close();
+        
     }
 }
